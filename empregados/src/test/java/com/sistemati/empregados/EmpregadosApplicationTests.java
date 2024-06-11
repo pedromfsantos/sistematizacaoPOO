f255d677303a659a3d1acb81ea8ad6ddf4a545d3
LG <LG@DESKTOP-LSS7SA7>
2024-06-11 11:19:11 -0300

    maven project for REST API

---
+++ empregados/src/test/java/com/sistemati/empregados/EmpregadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemati.empregados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpregadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
