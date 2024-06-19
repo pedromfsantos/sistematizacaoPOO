1519386f15d1a666e20a4a19b1b200fce89377fb
Gian <Gian@DESKTOP-2DNVAH5>
2024-06-19 11:52:21 -0300

    put method fixed. Add or remove a characteristic not implemented

--- empregados/src/main/java/com/sistemati/empregados/controllers/empregadosController.java
+++ empregados/src/main/java/com/sistemati/empregados/controllers/empregadosController.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,6 +23,8 @@ import com.sistemati.empregados.models.problemaSaudeModel;
 import com.sistemati.empregados.models.telefoneModel;
 import com.sistemati.empregados.repositories.alergiasRepository;
 import com.sistemati.empregados.repositories.empregadosRepository;
+import com.sistemati.empregados.repositories.problSaudeRepository;
+import com.sistemati.empregados.repositories.telefonesRepository;
 
 import jakarta.validation.Valid;
 
@@ -34,6 +36,13 @@ public class empregadosController {
 
 	@Autowired
 	alergiasRepository alergia_repository;
+	
+	@Autowired
+	telefonesRepository telefone_repository;
+	
+	@Autowired
+	problSaudeRepository problsaude_repository;
+	
 
 	@GetMapping("/empregados")
 	public ResponseEntity<List<empregadoModel>> listar() {
@@ -46,8 +55,6 @@ public class empregadosController {
 
 		var empregado_model = new empregadoModel(empregado_dto.getEmpregado(), empregado_dto.getEmail());
 
-		// BeanUtils.copyProperties(empregado_dto, empregado_model);
-
 		List<alergiaModel> alergias = new ArrayList<>();
 
 		for (alergiaModel alergiaIn : empregado_dto.getAlergia()) {
@@ -55,10 +62,8 @@ public class empregadosController {
 			alergiaModel alergia = new alergiaModel(alergiaIn.getAlergia());
 
 			alergia.setEmpregado(empregado_model);
-			System.out.println(alergia.getEmpregado().getId());
 
 			alergias.add(alergia);
-			System.out.println("passando");
 
 		}
 
@@ -82,12 +87,11 @@ public class empregadosController {
 
 			problemasSaude.add(problemaSaude);
 		}
-		
+
 		empregado_model.setAlergia(alergias);
 		empregado_model.setProblsaude(problemasSaude);
 		empregado_model.setTelefone(telefones);
-		
-	
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(empregados_repository.save(empregado_model));
 	}
 
@@ -103,12 +107,64 @@ public class empregadosController {
 	@PutMapping("/empregados/{id}")
 	public ResponseEntity<Object> atualizarEmpregado(@PathVariable(value = "id") Integer id,
 			@RequestBody @Valid empregadoDto empregado_dto) {
+
 		Optional<empregadoModel> empregado = empregados_repository.findById(id);
+
 		if (empregado.isEmpty()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Empregado não encontrado.");
 		}
+
 		var empregadoModel = empregado.get();
-		BeanUtils.copyProperties(empregado_dto, empregadoModel);
+
+		alergiaModel alergiaExample = new alergiaModel();
+		alergiaExample.setEmpregado(empregadoModel);
+
+		Example<alergiaModel> example = Example.of(alergiaExample);
+
+		List<alergiaModel> alergia = alergia_repository.findAll(example);
+
+			
+		for (int i = 0; i < empregado_dto.alergia().size(); i++) {
+			
+			var alergiaModel = alergia.get(i);
+			alergiaModel.setAlergia(empregado_dto.alergia().get(i).getAlergia());
+			// System.out.println(empregado_dto.alergia().get(i).getAlergia());
+
+		}
+		
+		
+		telefoneModel telefoneExample = new telefoneModel();
+		telefoneExample.setEmpregado(empregadoModel);
+
+		Example<telefoneModel> exampletel = Example.of(telefoneExample);
+
+		List<telefoneModel> telefone = telefone_repository.findAll(exampletel);
+		
+		for (int i = 0; i < telefone.size(); i++) {
+			var telefoneModel = telefone.get(i);
+			telefoneModel.setTelefone(empregado_dto.telefone().get(i).getTelefone());
+			// System.out.println(empregado_dto.alergia().get(i).getAlergia());
+
+		}
+		
+		problemaSaudeModel problSaudeExample = new problemaSaudeModel();
+		problSaudeExample.setEmpregado(empregadoModel);
+
+		Example<problemaSaudeModel> exampleProblSaude = Example.of(problSaudeExample);
+
+		List<problemaSaudeModel> problSaude = problsaude_repository.findAll(exampleProblSaude);
+		
+		for (int i = 0; i < problSaude.size(); i++) {
+			var problSaudeModel = problSaude.get(i);
+			problSaudeModel.setProblsaude(empregado_dto.problsaude().get(i).getProblsaude());
+			// System.out.println(empregado_dto.alergia().get(i).getAlergia());
+
+		}
+		
+		
+		 empregadoModel.setEmail(empregado_dto.email());
+		 empregadoModel.setEmpregado(empregado_dto.empregado());
+
 		return ResponseEntity.status(HttpStatus.OK).body(empregados_repository.save(empregadoModel));
 	}
 
@@ -119,7 +175,7 @@ public class empregadosController {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Empregado não encontrado.");
 		}
 		empregados_repository.delete(empregado.get());
-		return ResponseEntity.status(HttpStatus.OK).body("O epregado foi excluído da base de dados");
+		return ResponseEntity.status(HttpStatus.OK).body("O empregado foi excluído da base de dados");
 	}
 
 }
